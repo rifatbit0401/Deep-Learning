0b2cedae02c188be8b8da13b5c939e8f0b770dbf
rifatbit0401 <rifatcern@gmail.com>
2017-06-19 14:14:10 +0600

    chatbot working code

---
+++ chat_bot/chat.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 07 09:11:32 2017
+
+@author: Shreyans
+"""
+
+import os
+from scipy import spatial
+import numpy as np
+import gensim
+import nltk
+from keras.models import load_model
+
+import theano
+
+theano.config.optimizer = "None"
+
+model = load_model('LSTM5000.h5')
+mod = gensim.models.Word2Vec.load('/home/rifat/Desktop/projects/chat_bot/apnews_dbow/doc2vec.bin');
+while (True):
+    x = input("Enter the message:");
+    sentend = np.ones((300,), dtype=np.float32)
+
+    sent = nltk.word_tokenize(x.lower())
+    sentvec = [mod[w] for w in sent if w in mod.vocab]
+
+    sentvec[14:] = []
+    sentvec.append(sentend)
+    if len(sentvec) < 15:
+        for i in range(15 - len(sentvec)):
+            sentvec.append(sentend)
+    sentvec = np.array([sentvec])
+
+    predictions = model.predict(sentvec)
+    outputlist = [mod.most_similar([predictions[0][i]])[0][0] for i in range(15)]
+    output = ' '.join(outputlist)
+    print(output)
